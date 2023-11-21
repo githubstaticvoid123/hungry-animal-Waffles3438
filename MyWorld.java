a12e65860f61f1f690ab169e2f6dd8e98c859659
Waffles <waffles3438@gmail.com>
2023-11-21 08:58:46 -0500

    Added an imposter!!!
    AMONG US?!?!?!?!

--- MyWorld.java
+++ MyWorld.java
@@ -17,7 +17,9 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        Amongus red = new Amongus();
         Starfish star = new Starfish();
         addObject(star, 300, 200);
+        addObject(red, 100, 200);
     }
 }
