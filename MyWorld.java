ecb4b31531f71c1859013e8bdf4ed605c83315c4
Waffles <waffles3438@gmail.com>
2023-11-21 09:12:29 -0500

    idk why blue isnt working but there is a blue among us
    blue among us isnt working and im too lazy to fix it rn

--- MyWorld.java
+++ MyWorld.java
@@ -19,7 +19,9 @@ public class MyWorld extends World
         super(600, 400, 1);
         Amongus red = new Amongus();
         Starfish star = new Starfish();
+        Bluesus blue = new Bluesus();
         addObject(star, 300, 200);
         addObject(red, 100, 200);
+        addObject(blue, 200, 500);
     }
 }
