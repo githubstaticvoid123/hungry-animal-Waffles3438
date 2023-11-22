15a1d264a57fa2dff011f9bc0f0f4cd1bca817d3
Waffles <waffles3438@gmail.com>
2023-11-22 09:30:25 -0500

    Random spawn position for bread

--- MyWorld.java
+++ MyWorld.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.Random;
 
 /**
  * Write a description of class MyWorld here.
@@ -17,9 +18,11 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        Random rand = new Random(); 
+        int x = rand.nextInt(600); 
         Starfish star = new Starfish();
         Bread bread = new Bread();
         addObject(star, 300, 350);
-        addObject(bread, 300, 1);
+        addObject(bread, x, 1);
     }
 }
