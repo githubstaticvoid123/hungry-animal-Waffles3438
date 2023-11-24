f53b9c0d1fb6b53bf90c2de856672a40c0ca736b
Waffles <waffles3438@gmail.com>
2023-11-24 13:18:04 -0500

    Added game over

--- MyWorld.java
+++ MyWorld.java
@@ -32,6 +32,12 @@ public class MyWorld extends World
         createBread();
     }
     
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
     public void increaseScore()
     {
         score++;
