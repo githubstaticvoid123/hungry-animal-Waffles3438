866bebdc51f29fe2087505652d6852217d2a59df
Waffles <waffles3438@gmail.com>
2023-11-28 08:46:38 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -38,7 +38,7 @@ public class MyWorld extends World
     public void gameOver()
     {
         Label gameOverLabel = new Label("Game Over", 100);
-        addObject(gameOverLabel, 300, 200);
+        addObject(gameOverLabel, x/2, y/2);
     }
     
     public void increaseScore()
