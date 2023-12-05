b934267fb63f13ebd25d95af4fc3c895177fa288
Waffles <waffles3438@gmail.com>
2023-12-05 13:14:38 -0500

    fixed replay feature

--- GameOver.java
+++ GameOver.java
@@ -19,9 +19,13 @@ public class GameOver extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         totalScore = new Label("Your score: " + MyWorld.score, 40);
-        addObject(totalScore, 300, 275);
+        addObject(totalScore, 300, 300);
         playAgain = new Label("Press space to play again", 40);
         addObject(playAgain, 300, 350);
+    }
+    
+    public void act()
+    {
         if(Greenfoot.isKeyDown("space"))
         {
             MyWorld gameWorld = new MyWorld();
